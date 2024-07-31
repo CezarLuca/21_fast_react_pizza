a4e7de1c0abc417ed660e6d06f52546bc7044c38
Cezar Luca <cezar.luca@hotmail.com>
2024-07-31 21:31:53 +0200

    Optimized Menu component to work with React Fast Refresh

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import Home from "./ui/Home";
+// import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Menu from "./features/menu/Menu";
+import menuLoader from "./features/menu/MenuLoader";
 import Cart from "./features/cart/Cart";
 import CreateOrder from "./features/order/CreateOrder";
 import Order from "./features/order/Order";
@@ -18,6 +20,7 @@ const router = createBrowserRouter([
             {
                 path: "/menu",
                 element: <Menu />,
+                loader: menuLoader,
             },
             {
                 path: "/cart",
