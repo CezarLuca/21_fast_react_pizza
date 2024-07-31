06d9375964a58fe91ea0cd0d429a68fd00f14912
Cezar Luca <cezar.luca@hotmail.com>
2024-07-31 16:56:48 +0200

    Building the App Layout done

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+    return (
+        <div>
+            <Link to="/">Fast React Pizza Co.</Link>
+            <p>To Be Done Later</p>
+        </div>
+    );
+}
+
+export default Header;
