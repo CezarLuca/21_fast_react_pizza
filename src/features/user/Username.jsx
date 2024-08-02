3d36fdc426a4180853e1babd07406b214b768c81
Cezar Luca <cezar.luca@hotmail.com>
2024-08-02 15:55:37 +0200

    Styling Color and Text with Tailwind CSS done

---
+++ src/features/user/Username.jsx
@@ -0,0 +1,9 @@
+function Username() {
+    return (
+        <div className="text-sm font-semibold">
+            Username To Be Automated With Redux
+        </div>
+    );
+}
+
+export default Username;
