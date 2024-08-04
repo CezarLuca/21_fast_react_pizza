37484a281d373ab1610f63ff9c75cc550e73e962
Cezar Luca <cezar.luca@hotmail.com>
2024-08-05 01:55:15 +0200

    Styling the Cart done

--- src/features/cart/CartItem.jsx
+++ src/features/cart/CartItem.jsx
@@ -1,16 +1,32 @@
-function CartItem({ item }) {
-  const { pizzaId, name, quantity, totalPrice } = item;
+import PropTypes from "prop-types";
+import { formatCurrency } from "../../utils/helpers";
+import CustomButton from "../../ui/CustomButton";
 
-  return (
-    <li>
-      <p>
-        {quantity}&times; {name}
-      </p>
-      <div>
-        <p>{formatCurrency(totalPrice)}</p>
-      </div>
-    </li>
-  );
+function CartItem({ item }) {
+    const { pizzaId, name, quantity, totalPrice } = item;
+
+    return (
+        <li className="py-3 sm:flex sm:items-center sm:justify-between">
+            <p className="mb-1 sm:mb-0">
+                {quantity}&times; {name}
+            </p>
+            <div className="flex items-center justify-between sm:gap-6">
+                <p className="text-sm font-bold">
+                    {formatCurrency(totalPrice)}
+                </p>
+                <CustomButton type="small">Remove</CustomButton>
+            </div>
+        </li>
+    );
 }
+
+CartItem.propTypes = {
+    item: PropTypes.shape({
+        pizzaId: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        quantity: PropTypes.number.isRequired,
+        totalPrice: PropTypes.number.isRequired,
+    }).isRequired,
+};
 
 export default CartItem;
--- src/ui/CustomButton.jsx
+++ src/ui/CustomButton.jsx
@@ -8,6 +8,8 @@ function CustomButton({ children, disabled, to, type }) {
     const styles = {
         primary: `${baseStyles} px-4 py-3 md:px-6 md:py-4`,
         small: `${baseStyles} px-4 py-2 md:px-5 md:py-2.5 text-xs`,
+        secondary:
+            "inline-block rounded-full border-2 border-stone-300 px-4 py-2.5 font-semibold uppercase tracking-wide text-stone-500 transition-colors duration-300 hover:bg-stone-300 hover:text-stone-600 focus:bg-stone-300 focus:text-stone-600 focus:outline-none focus:ring focus:ring-stone-200 focus:ring-offset-2 disabled:cursor-not-allowed disabled:bg-slate-200 md:px-6 md:py-3.5",
     };
 
     // const className =
